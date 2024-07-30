dd340e8b92381657f7d26aea71f1b28a84299702
Ngo Duc Canh <ngoduccanh19@gmail.com>
2024-07-30 15:45:22 +0700

    comment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from evaluate import execute
 from pose_parser import pose_parse
 
 st.title("Virtual Try ON")
-#ảnh có sẵn để chọn
+# ảnh có sẵn để chọn
 cloth1 = Image.open('./Database/val/cloth/002337_1.jpg')
 cloth2 = Image.open('./Database/val/cloth/001401_1.jpg')
 cloth3 = Image.open('./Database/val/cloth/003086_1.jpg')
@@ -16,7 +16,7 @@ st.sidebar.image(cloth1, caption="002337", width=100, use_column_width=False)
 st.sidebar.image(cloth2, caption="002599", width=100, use_column_width=False)
 st.sidebar.image(cloth3, caption="003086", width=100, use_column_width=False)
 
-#Tải ảnh người dùng lên
+# Tải ảnh người dùng lên (Chỗ này cần sửa này Cường ơi vì đang tải ảnh nên qua thằng treamlit)
 uploaded_person = st.file_uploader("Upload a Photo", type="jpg")
 user_input = st.text_input("Enter the User Name... eg sourav")
 selected = st.selectbox('Select the Item Id:', [
@@ -43,37 +43,34 @@ if uploaded_person is not None and user_input is not '' and selected is not '':
     st.write("Please click the Click Button after Pose pairs and Masks are generated")
 
 if st.button('Execute'):
-    f = open("./Database/val_pairs.txt" , "w")    
+    f = open("./Database/val_pairs.txt", "w")
     f.write(user_input+".jpg "+selected+"_1.jpg")
     f.close()
     # Chỗ này chạy model này, còn lại ở dưới chỉ là chỉnh output đầu ra thôi
     predict()
     from PIL import Image
     im = Image.open("./output/second/TOM/val/" + selected + "_1.jpg")
-    width, height = im.size  
-  
-# # Setting the points for cropped image  
+    width, height = im.size
+
+# # Setting the points for cropped image
     left = width / 3
     top = 2 * height / 3
     right = 2 * width / 3
     bottom = height
-  
-# # Cropped image of above dimension  
-# # (It will not change orginal image)  
-    im1 = im.crop((left, top, right, bottom)) 
-    newsize = (256, 256) 
-    im1 = im1.resize(newsize) 
+
+# # Cropped image of above dimension
+# # (It will not change orginal image)
+    im1 = im.crop((left, top, right, bottom))
+    newsize = (256, 256)
+    im1 = im1.resize(newsize)
 
     # Sử dụng bộ lọc sắc nét
     im1 = im1.filter(ImageFilter.SHARPEN)
-# # Shows the image in image viewer  
+# # Shows the image in image viewer
     im1.save("./output/second/TOM/val/" + selected + "_1.jpg")
     execute_bar = st.progress(0)
     for percent_complete in range(100):
         time.sleep(0.08)
         execute_bar.progress(percent_complete + 1)
     result = Image.open("./output/second/TOM/val/" + selected + "_1.jpg")
-    st.image(result , caption="Result" , width=200 , use_column_width=False)
-
-
-
+    st.image(result, caption="Result", width=200, use_column_width=False)
